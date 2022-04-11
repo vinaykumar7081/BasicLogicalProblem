3889e2e02b244e170d5d8805014ea5142784f1d9
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-11 14:09:17 +0530

    [VINAY] Add: Checking the Alphabet Vawel And Consonents

--- BasicLogicalProgramming/Program.cs
+++ BasicLogicalProgramming/Program.cs
@@ -6,7 +6,7 @@ public class Program
     public static void Main(String[] args)
     {
         Console.WriteLine(" ********* Welcome in The Basic Logical Programming World ********");
-        Console.WriteLine("flip coin number 1\n Square of Two 2\n Leap Year 3\n Harmonic Number 4\n Prime Factor 5\n Quotient and Remainder 6\n Swapping 7\n EvenAnd Odd 8\n");
+        Console.WriteLine("flip coin number 1\n Square of Two 2\n Leap Year 3\n Harmonic Number 4\n Prime Factor 5\n Quotient and Remainder 6\n Swapping 7\n EvenAnd Odd 8\n VowelAndConsonents 9\n");
         bool check=true;
         while (check)
         { 
@@ -45,6 +45,10 @@ public class Program
                 case 8:
                     EvenOrOdd  checking = new EvenOrOdd ();
                     checking.Check ();
+                    break;
+                case 9:
+                   VowelConsonents  charector = new VowelConsonents ();
+                    charector.VowelAndConsonent ();
                     break;
                 default : 
                     Console.WriteLine("Please Enter the valid Number to Execution the Your Program");
---
+++ BasicLogicalProgramming/VowelConsonents.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgramming
+{
+    internal class VowelConsonents
+    {
+        char ch;
+        
+        public void VowelAndConsonent()
+        {
+            Console.WriteLine("Please Enter the Alphabets:->");
+            ch = Console.ReadKey().KeyChar;
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                Console.WriteLine("Entered by End user Character is vowel");
+            }
+            else
+            {
+                Console.WriteLine("Entered by End user Character is Consonent");
+            }
+
+
+        }
+    }
+}
