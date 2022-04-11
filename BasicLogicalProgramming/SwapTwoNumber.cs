3a9ef387cc936b1f897ac5d2b0947cd15a09b935
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-11 13:42:02 +0530

    [VINAY] Add: Swapping of two number without using Third Variable

---
+++ BasicLogicalProgramming/SwapTwoNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgramming
+{
+    public  class SwapTwoNumber
+    {
+        public void Swap()
+        {
+            Console.WriteLine("Enter a first number:->");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a Second number:->");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            Console.WriteLine("After Swapping First Number " + num1);
+            Console.WriteLine("After Swapping Second Number " + num2);
+
+
+        }
+    }
+}
