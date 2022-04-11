db48619c37d896f781dd6551dd63bb7eb1a17f78
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-11 13:20:20 +0530

    [VINAy] Add: Displaying Quotien and Remainder Of a  number

---
+++ BasicLogicalProgramming/QuotientAndRemainder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgramming
+{
+    public class QuotientAndRemainder
+    {
+        int remeinder = 0;
+        int quotient = 0;
+        public void RemienderAndQuotient()
+        {
+            Console.WriteLine("Enter a number:->");
+            int number = Convert.ToInt32(Console.ReadLine());
+            remeinder = number % 10;
+            quotient = number / 10;
+            Console.WriteLine("Remiender of that number is:->" + remeinder);
+            Console.WriteLine("Quotient of that number is:->" + quotient);
+        }
+
+    }
+}
