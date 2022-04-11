ced5b353e95e1a3b07dce8160b8b060cc67ca1d1
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-11 12:40:54 +0530

    [VINAY] Add: Displaying Square of the two

---
+++ BasicLogicalProgramming/DisplayinSquare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogicalProgramming
+{
+    internal class DisplayinSquare
+    {
+        public void SquareOfTwo()
+        {
+
+            Console.WriteLine("Hello Sir Enter a number:->");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                int num = (int)Math.Pow(2, i);
+                Console.WriteLine("2" + " " + "^" + " " + i + "=" + num);
+            }
+
+
+        }
+    }
+}
--- BasicLogicalProgramming/Program.cs
+++ BasicLogicalProgramming/Program.cs
@@ -6,7 +6,7 @@ public class Program
     public static void Main(String[] args)
     {
         Console.WriteLine(" ********* Welcome in The Basic Logical Programming World ********");
-        Console.WriteLine("flip coin number 1\n");
+        Console.WriteLine("flip coin number 1\n,Square of Two 2\n");
         bool check=true;
         while (check)
         { 
@@ -18,8 +18,12 @@ public class Program
                     FlipCoin percentage = new FlipCoin();
                     percentage.FlipCoins();
                     break;
-                     default : 
-                    Console.WriteLine("PLease Enter the valid Number to Execution the Your Program");
+                case 2:
+                    DisplayinSquare  square = new DisplayinSquare();
+                    square.SquareOfTwo();
+                    break;
+                default : 
+                    Console.WriteLine("Please Enter the valid Number to Execution the Your Program");
                     break;
             }
         }
