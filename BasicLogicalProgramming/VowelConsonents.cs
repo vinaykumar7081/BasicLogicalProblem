f54f8d49f646e2bc1a061ab84c18d1cab320a9d7
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-12 16:36:16 +0530

    [VINAY] Add: Modified the vowel And Consonents Problem

--- BasicLogicalProgramming/VowelConsonents.cs
+++ BasicLogicalProgramming/VowelConsonents.cs
@@ -8,22 +8,22 @@ namespace BasicLogicalProgramming
 {
     internal class VowelConsonents
     {
-        char ch;
-        
         public void VowelAndConsonent()
         {
-            Console.WriteLine("Please Enter the Alphabets:->");
-            ch = Console.ReadKey().KeyChar;
+            Console.WriteLine("Please Enter the word:->");
+            char ch = Console.ReadKey().KeyChar;
             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
             {
-                Console.WriteLine("Entered by End user Character is vowel");
+                Console.WriteLine("Entered by End user Character is vowel in Lower case");
+            }
+            else if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("Entered by End user Character is vowel in Upper case");
             }
             else
             {
-                Console.WriteLine("Entered by End user Character is Consonent");
+                Console.WriteLine("Entered by End user Character is Consonents");
             }
-
-
         }
     }
 }
